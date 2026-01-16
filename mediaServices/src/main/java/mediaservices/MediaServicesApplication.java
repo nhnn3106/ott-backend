478f7f738ee4161acf57c99f79f506b040cfb20c
unknown <nnguyennhan365@gmail.com>
2026-01-16 21:27:28 +0700

    init media service

---
+++ mediaServices/src/main/java/mediaservices/MediaServicesApplication.java
@@ -0,0 +1,13 @@
+package mediaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaServicesApplication.class, args);
+	}
+
+}
---
+++ mediaServices/src/test/java/mediaservices/MediaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mediaservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
