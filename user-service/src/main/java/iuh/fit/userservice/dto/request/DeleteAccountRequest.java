7553a237429385d12147fbafb82737ddc159f320
ChucNguyen <nguyenxuanchuc111@gmail.com>
2026-03-10 03:25:16 +0700

    fix user-service

---
+++ user-service/src/main/java/iuh/fit/userservice/dto/request/DeleteAccountRequest.java
@@ -0,0 +1,19 @@
+package iuh.fit.userservice.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteAccountRequest {
+    @NotBlank(message = "OTP is required")
+    private String otp;
+
+    private String password;
+    private String ipAddress;
+}
